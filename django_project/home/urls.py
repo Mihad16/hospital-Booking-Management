6568945adb70da34676bde204dadcb0d893fad32
lenova <mihaddeskto@gmail.com>
2026-04-17 17:57:51 +0530

    added urls

--- django_project/home/urls.py
+++ django_project/home/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
  
     path('',views.index),
+     path('doctors/',views.doctors),
+      path('about/',views.about),
+       path('contact/',views.contact),
+        path('department/',views.department),
+        path('booking/',views.booking),
 ]   
