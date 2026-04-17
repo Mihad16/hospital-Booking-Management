0c53a6ac5ee4e423f00ba58269802bcb86a83b15
lenova <mihaddeskto@gmail.com>
2026-04-17 18:21:41 +0530

    template added

--- django_project/home/views.py
+++ django_project/home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+numbers ={
+    'num1': 0,
+    
+ }
+ 
 def index(request):
-   return render(request,'index.html')
+   return render(request,'index.html', numbers)
 
 
 def doctors(request):
